712f8b08e2b33278c3d484906e33f1c4cc9cc54a
jcobian <jcobian@civisanalytics.com>
2015-10-05 20:51:22 -0500

    basic routing

--- sports/sports.go
+++ sports/sports.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 )
 
+func playersHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/players/"):]
+	fmt.Fprintf(w, "<h1>%s</h1>", title)
+}
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Yoohoo. Big summer blowout: %s!", r.URL.Path[1:])
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/players/", playersHandler)
 	http.ListenAndServe(":8080", nil)
 }
